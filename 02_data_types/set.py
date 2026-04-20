e9bd1d9e7ca1b3bec63e4d7a5d227c8bdfe7e85d
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-04-20 23:58:25 +0530

    set

---
+++ 02_data_types/set.py
@@ -0,0 +1,14 @@
+my_phones = {"Samsung s9+", "Iphone 14+", "Iphone 13"}
+frind_phones = {"Google pixel", "Iphone 13"}
+
+
+all_phones = my_phones | frind_phones
+print(all_phones)
+
+common_phones = my_phones & frind_phones
+print(common_phones)
+
+only_my_phones_uncommon  = my_phones - frind_phones
+print(only_my_phones_uncommon)
+
+print(f"Is 'Iphone 14+' in friend phone ? {'Iphone 14+' in frind_phones}")
