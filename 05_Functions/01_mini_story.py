ebe64b0d809a3ab3cece700b53cc35032b1cbd25
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-04-26 22:49:30 +0530

    Functions - mini story 1

---
+++ 05_Functions/01_mini_story.py
@@ -0,0 +1,7 @@
+
+def print_order(name, chai_type):
+    print(f"{chai_type} for {name}")
+
+print_order("Karthik", "Green tea")
+print_order("Aditya", "Black tea")
+print_order("Neeraj", "Ginger tea")
