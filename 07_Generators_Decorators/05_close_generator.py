159453fdebfee28eed819db0737d63156218613d
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-04-29 19:21:39 +0530

    Generators - Close Generators

---
+++ 07_Generators_Decorators/05_close_generator.py
@@ -0,0 +1,12 @@
+def chai_stall():
+    try:
+        while True:
+            order = yield "Waiting for chai order"
+    except GeneratorExit: # When ever python finished the script it automatically uses .close() which throws a special exception named "GeneratorExit" exception. This # next(stall) only "Stall closed, No more chai" gets printed
+        print("Stall closed, No more chai")
+
+stall = chai_stall()
+# next(stall) only "Stall closed, No more chai" gets printed
+print(next(stall)) # Both the lines get printed 
+
+stall.close() # Clean up
